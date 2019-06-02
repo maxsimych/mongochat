04d87c0d49d7bcce461b5cdaae3b86838f844d31
maxsimych <maxsimych@gmail.com>
2019-06-02 18:51:41 +0300

    first release

--- src/server.js
+++ src/server.js
@@ -8,16 +8,16 @@ mongo.connect('mongodb://localhost/mongochat', (err, db) => {
     throw err;
   };
 
-  console.log('MongoDB connected!');
+  console.log(`MongoDB connected!`);
   client.on('connection', (socket) => {
     const chat = db.collection('chats');
     const sendStatus = (s) => {
       socket.emit('status', s);
-    }
+    };
     chat.find().limit(100).sort({_id:1}).toArray((err,res) =>{
       if (err) {
         throw err;
-      }
+      };
       socket.emit('output', res);
     });
     socket.on('input', (data) => {
@@ -32,13 +32,13 @@ mongo.connect('mongodb://localhost/mongochat', (err, db) => {
           sendStatus({
             message: 'message sent',
             clear: true
-          })
+          });
         });
       };
     });
     socket.on('clear', (data) => {
       chat.remove({},() => {
-        socket.emit('cleared');
+        client.emit('cleared');
       });
     });
   });
