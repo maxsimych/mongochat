3ff12b75e7a13904f5532ed1cb7e8a49da1f571f
maxsimych <maxsimych@gmail.com>
2019-06-02 20:13:59 +0300

    Promisified

--- src/server.js
+++ src/server.js
@@ -1,45 +1,50 @@
 import mongodb from 'mongodb'
 import socketIo from 'socket.io';
 const mongo = mongodb.MongoClient;
-const client = socketIo.listen(4000).sockets;
+const io = socketIo.listen(4000).sockets;
 
-mongo.connect('mongodb://localhost/mongochat', (err, db) => {
-  if(err) {
-    throw err;
-  };
-
-  console.log(`MongoDB connected!`);
-  client.on('connection', (socket) => {
-    const chat = db.collection('chats');
-    const sendStatus = (s) => {
-      socket.emit('status', s);
-    };
-    chat.find().limit(100).sort({_id:1}).toArray((err,res) =>{
-      if (err) {
-        throw err;
-      };
-      socket.emit('output', res);
-    });
-    socket.on('input', (data) => {
-      const name = data.name;
-      const message = data.message;
-
-      if(name == ''|| message == '') {
-        sendStatus('Please enter a name and message');
-      } else {
-        chat.insert({name: name, message: message}, () => {
-          client.emit('output', [data]);
-          sendStatus({
-            message: 'message sent',
-            clear: true
-          });
+(async () => {
+  try {
+    const db = (await mongo.connect('mongodb://localhost/mongochat', { useNewUrlParser: true })).db('mongochat');
+    console.log(`MongoDB connected!`);
+    io.on('connection', async (socket) => {
+      try {
+        const chat = db.collection('chats');
+        const sendStatus = (s) => {
+          socket.emit('status', s);
+        };
+        const res = await chat.find().limit(100).sort({_id:1}).toArray();
+        socket.on('input', async (data) => {
+          try {
+            const name = data.name;
+            const message = data.message;
+            if(name == ''|| message == '') {
+              sendStatus('Please enter a name and message');
+            } else {
+              await chat.insert({name: name, message: message});
+              io.emit('output', [data]);
+              sendStatus({
+                message: 'message sent',
+                clear: true
+              });
+            };
+          } catch(error) {
+            throw error;
+          };
         });
+        socket.on('clear', async () => {
+          try {
+            await chat.remove({});
+            io.emit('cleared');
+          } catch(error) {
+            throw error;
+          };
+        });
+      } catch(error) {
+        throw error;
       };
-    });
-    socket.on('clear', (data) => {
-      chat.remove({},() => {
-        client.emit('cleared');
-      });
-    });
-  });
-});
+    }); 
+  } catch(error) {
+    console.log(`Error: ${error}`);
+  };
+})();
